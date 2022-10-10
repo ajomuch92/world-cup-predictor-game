b9493a99f9b7f51868577d798d914071e38224eb
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-09 18:02:49 -0600

    refactor: 💡 Adding route for league screen

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import 'a-flexbox/a-flexbox.css';
 import '@picocss/pico';
 import 'toastify-js/src/toastify.css';
 import Predictor from './views/Predictor';
+import League from './views/League';
 
 const router = createBrowserRouter([
   {
@@ -19,7 +20,11 @@ const router = createBrowserRouter([
   {
     path: '/home',
     element: <Predictor />
-  }
+  },
+  {
+    path: '/leagues',
+    element: <League />
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
